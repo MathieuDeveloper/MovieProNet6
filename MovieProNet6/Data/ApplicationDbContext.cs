94bee3fe14bf0166a687e3c5af2998f711946034
MathieuDeveloper <topfermathieu@yahoo.fr>
2022-05-26 12:20:29 +0200

    Library M and V C create

--- MovieProNet6/Data/ApplicationDbContext.cs
+++ MovieProNet6/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using MovieProNet6.Models.Database;
+using MovieProNet6.Models.CustomCollection;
 
 namespace MovieProNet6.Data
 {
@@ -14,5 +15,6 @@ namespace MovieProNet6.Data
         public DbSet<Collection> Collection { get; set; }
         public DbSet<Movie> Movie { get; set; }
         public DbSet<MovieCollection> MovieCollection { get; set; }
+        public DbSet<MovieProNet6.Models.CustomCollection.Library>? Library { get; set; }
     }
 }
---
+++ MovieProNet6/Models/CustomCollection/Library.cs
@@ -0,0 +1,14 @@
+﻿using MovieProNet6.Models.Database;
+
+namespace MovieProNet6.Models.CustomCollection
+{
+    public class Library
+    {
+        public int Id { get; set; }
+        public int CollectionId { get; set; }
+        public int MovieId { get; set; }        
+
+        public Collection Collection { get; set; }
+        public Movie Movie { get; set; }
+    }
+}
