789120048f847416dfe555a8b39f8ba8193ab18a
MathieuDeveloper <topfermathieu@yahoo.fr>
2022-06-20 23:29:10 +0200

    Update TMDBMovieService.cs

--- MovieProNet6/Services/TMDBMovieService.cs
+++ MovieProNet6/Services/TMDBMovieService.cs
@@ -116,5 +116,43 @@ namespace MovieProNet6.Services
 
             return movieSearch;
         }
+
+        //Mathieu essaie créer nouvelle class pour importer movie:
+        public async Task<ImportMovie> ImportMovieAsync(string title)
+        {
+
+            //Step 1: Setup a default instance of MovieSearch
+            MovieSearch movieSearch = new();
+
+            //Step 2: Assemble the full request uri string
+            var query = $"{_appSettings.TMDBSettings.BaseUrl}/movie/{category}";
+
+            var queryParams = new Dictionary<string, string>()
+            {
+                { "api_key", _appSettings.MovieProSettings.TmDbApiKey },
+                { "language", _appSettings.TMDBSettings.QueryOptions.Language },
+                { "page", _appSettings.TMDBSettings.QueryOptions.Page }
+            };
+
+            var requestUri = QueryHelpers.AddQueryString(query, queryParams);
+
+            //Step 3: Create a client and execute the request
+            var client = _httpClient.CreateClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+            var response = await client.SendAsync(request);
+
+            //Step 4: Return the MovieSearch object
+            if (response.IsSuccessStatusCode)
+            {
+                var dcjs = new DataContractJsonSerializer(typeof(MovieSearch));
+                using var responseStream = await response.Content.ReadAsStreamAsync();
+                movieSearch = (MovieSearch)dcjs.ReadObject(responseStream);
+                movieSearch.results = movieSearch.results.Take(count).ToArray();
+                movieSearch.results.ToList().ForEach(r => r.poster_path = $"{_appSettings.TMDBSettings.BaseImagePath}/{_appSettings.MovieProSettings.DefaultPosterSize}/{r.poster_path}");
+            }
+
+            return movieSearch;
+        }
+
     }
 }
